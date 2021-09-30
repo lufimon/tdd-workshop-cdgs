c5fe4f826b25d64b97634e6b9c810f401a35ca45
TANUPHONG PHOEMTAWEESATISUH(CDGS-ADD) <tanuphong.p@cdg.co.th>
2021-09-30 11:57:45 +0700

    test case03 ตัวแรกต้องเป็น [ (include) โดยที่ input = (1,5], result = false

--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -23,4 +23,14 @@ public class MyRangeTest {
         boolean result = range.startWithInclude();
         assertTrue(result);
     }
+
+    @Test
+    @DisplayName("ตัวแรกต้องเป็น [ (include) โดยที่ input = (1,5], result = false")
+    public void case03() {
+        // Arrange
+        String input = "(1,5]";
+        MyRange range = new MyRange(input);
+        boolean result = range.startWithInclude();
+        assertFalse(result);
+    }
 }
