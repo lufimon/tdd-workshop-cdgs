6d62ce9e26575d8af60bdf95b945920e1c6700a1
TANUPHONG PHOEMTAWEESATISUH(CDGS-ADD) <tanuphong.p@cdg.co.th>
2021-09-30 12:02:49 +0700

    test case08 ตัวแรกต้องเป็น ( (exclude) โดยที่ input = (1,5), result = true

--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -73,4 +73,14 @@ public class MyRangeTest {
         boolean result = range.startWithExclude();
         assertTrue(result);
     }
+
+    @Test
+    @DisplayName("ตัวแรกต้องเป็น ( (exclude) โดยที่ input = (1,5), result = true")
+    public void case08() {
+        // Arrange
+        String input = "(1,5)";
+        MyRange range = new MyRange(input);
+        boolean result = range.startWithExclude();
+        assertTrue(result);
+    }
 }
