17a68f3b2017d1a570a22b63f0a0b90672ad082c
TANUPHONG PHOEMTAWEESATISUH(CDGS-ADD) <tanuphong.p@cdg.co.th>
2021-09-30 13:47:30 +0700

    update, test case02 use junit5 assertThrows

--- src/test/java/MyRangeFailureTest.java
+++ src/test/java/MyRangeFailureTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyRangeFailureTest {
     @Test
@@ -16,4 +16,13 @@ public class MyRangeFailureTest {
                 fail("Error message ผิด : " + e.getMessage());
             }
         }
-    }}
+    }
+
+    @Test
+    @DisplayName("ขึ้นต้นด้วย [, ( เท่านั้น input 1,5]")
+    public void case02() {
+        MyRange range = new MyRange("1,5]");
+        Exception e = assertThrows(InputInvalidException.class, range::validate);
+        assertEquals("Input error", e.getMessage());
+    }
+}
