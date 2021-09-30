4e21168bb6b36325637266f9358b12bcb2582974
TANUPHONG PHOEMTAWEESATISUH(CDGS-ADD) <tanuphong.p@cdg.co.th>
2021-09-30 12:09:12 +0700

    test case14 ตัวท้ายต้องเป็น ) (exclude) โดยที่ input = [1,5), result = true

--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -133,4 +133,14 @@ public class MyRangeTest {
         boolean result = range.endWithExclude();
         assertFalse(result);
     }
+
+    @Test
+    @DisplayName("ตัวท้ายต้องเป็น ) (exclude) โดยที่ input = [1,5), result = true")
+    public void case14() {
+        // Arrange
+        String input = "[1,5)";
+        MyRange range = new MyRange(input);
+        boolean result = range.endWithExclude();
+        assertTrue(result);
+    }
 }
